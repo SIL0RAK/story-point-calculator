a6c08ccc691f190a78de716d8811378c0e9db0eb
karolis krusinskas <nfq002621@nfq002621.aforeman.nfq.lt>
2023-01-24 11:14:49 +0200

    fix: article

--- pages/educate.vue
+++ pages/educate.vue
@@ -21,7 +21,7 @@
       </template>
       <template v-slot:content>
         How many tricky algorithms or query's you will have to write.
-        <p class="mt-3">
+        <div class="mt-3">
           <b>Example:</b>
           <ul class="list-disc pl-5 mt-3">
             <li>Task requires to write tricky algorithms of queries (high complexity).</li>
@@ -30,7 +30,7 @@
           <br>
           First task is very hard and will require a lot of brain cells to do.
           Second task is easy and don't require knowledge structure.
-        </p>
+        </div>
       </template>
     </Article>
 
@@ -44,7 +44,7 @@
         >
       </template>
       <template v-slot:content>
-        <p class="mt-3">
+        <div class="mt-3">
           <b>Example:</b>
           <ul class="list-disc pl-5 mt-3">
             <li>Task requires to update service in controller (low effort).</li>
@@ -52,7 +52,7 @@
           </ul>
           <br>
           Both of these tasks have same Complexity, but requires different Effort.
-        </p>
+        </div>
       </template>
     </Article>
 
@@ -66,7 +66,7 @@
         >
       </template>
       <template v-slot:content>
-        <p class="mt-3">
+        <div class="mt-3">
           <b>Example:</b>
           <ul class="list-disc pl-5 mt-3">
             <li>Task requires to update service in project that you created (low uncertainty).</li>
@@ -76,20 +76,8 @@
           These tasks are similar in sense that they require to update service,
           but in first example it is more clear what to do, because you are familiar with project.
           In second example you are not sure what how/where changes needs to be done so Uncertainty is high.
-        </p>
+        </div>
       </template>
     </Article>
-
-    <div>
-      <p class="mt-3">
-          <b>Example:</b>
-          <ul class="list-disc pl-5 mt-3">
-            <li>Task requires to update service in controller (low effort).</li>
-            <li>Task requires to update same service in 4 different repositories (high effort).</li>
-          </ul>
-          <br>
-          Both of these tasks have same Complexity, but requires different Effort.
-        </p>
-    </div>
   </Container>
 </template>
